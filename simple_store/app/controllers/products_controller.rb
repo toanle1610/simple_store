f3e9fc74ed153a0ad6282a471f68bd5764294ba3
Tony Le <tle68@academic.rrc.ca>
2019-09-18 18:27:49 -0500

    Updated index view to have stats

--- simple_store/app/controllers/products_controller.rb
+++ simple_store/app/controllers/products_controller.rb
@@ -3,5 +3,8 @@
 class ProductsController < ApplicationController
   def index
     @products = Product.all
+    @totalproducts = Product.all.count
+    @averageprice = Product.average(:price)
+    @totalitems = Product.sum(:stock_quantity)
   end
 end
