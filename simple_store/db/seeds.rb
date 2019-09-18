3e5d02c295f30067133684dcc80053c883170b39
Tony Le <tle68@academic.rrc.ca>
2019-09-18 17:32:02 -0500

    Updated seeds script

--- simple_store/db/seeds.rb
+++ simple_store/db/seeds.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+(1..676).each do |_i|
+  Product.create(
+    title: Faker::Book.title,
+    description: Faker::Book.publisher,
+    price: Faker::Number.decimal(l_digits: 2),
+    stock_quantity: Faker::Number.within(range: 0..50)
+  )
+end
